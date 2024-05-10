47499e4468a3c83bb05ed215f72341fe9948e94f
AB7zz <abhinavcv007@gmail.com>
2024-05-10 20:43:09 +0530

    changed shell script

--- auto.sh
+++ auto.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+g++ -o z--.exe z--.cpp
 
-
-# lexer.cpp
-g++ src/lexer.cpp -o src/lexer.exe
-if [ $? -eq 0 ]; then
-    echo "Running lexer..."
-    ./src/lexer.exe
-else
-    echo "Compilation failed for lexer.cpp"
-    exit 1
-fi
-
-# parser.cpp
-# g++ parser.cpp -o parser
-# if [ $? -eq 0 ]; then
-#   echo "Running parser..."
-#   ./parser
-# else
-#   echo "Compilation failed for parser.cpp"
-#   exit 1
-# fi
+z-- test.z
